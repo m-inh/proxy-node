0452957e443e3fd28d6cd7105a4e954a1106089f
Minh Nguyen <tienminh.uet@gmail.com>
2018-08-31 11:26:42 +0700

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const morgan = require('morgan');
+const request = require('request');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+const demoTargetLink = 'http://localhost:3000/s1';
+
+app.use(morgan('dev'));
+
+app.get('/api/tmdt/label', (req, res) => {
+	const {tracking_id} = req.query;
+
+	//return res.json({demoTargetLink});
+	request(demoTargetLink).pipe(res);
+});
+
+app.get('/s1', (req, res) => {
+	res.sendFile(path.resolve(__dirname, './label.html'));
+})
+
+app.listen(port, (err) => {
+	if (err) throw err;
+	console.log('server is listening on', port);	
+});
